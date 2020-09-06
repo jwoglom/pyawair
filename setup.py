ab275a2cabf003318af6d3c932df2da27cdfff4c
James Woglom <j@wogloms.net>
2020-09-05 22:34:26 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setuptools.setup(
     version="0.0.1",
     author="jwoglom forked from harperreed",
     description="A very simple python class to access the (private) awair api",
-    long_description="",
+    long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
     url="https://github.com/jwoglom/pyawair",
-    packages=setuptools.find_packages(),
+    packages=['pyawair'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
